1b640f4da1c7cbcbebed0c0fbae2d6b7539b223b
BittuKumar726 <krbittu2002@gmail.com>
2020-03-02 22:26:39 +0530

    first commit

---
+++ java/ASCII_VALUES/ASCII1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        // fill the code;
+      Scanner sc = new Scanner(System.in);
+      char ch = sc.next().charAt(0);
+      int c=(int)ch;
+      System.out.println(c);
+      
+    }
+}
---
+++ java/ASCII_VALUES/ASCII1.md
@@ -0,0 +1,9 @@
+Unicode Values-I
+Write a program to get a character as input and print its Unicode value. When a character is entered, the character itself is not stored. Instead, a numeric value(Unicode value) is stored.
+
+Sample Input :
+g
+
+Sample Output:
+103
+
